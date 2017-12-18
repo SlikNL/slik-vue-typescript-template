16d6659a95c11767ee3136510c737231ed2a1b8c
张韧 <zhangren@meituan.com>
2017-12-18 19:39:54 +0800

    router

--- template/src/App.vue
+++ template/src/App.vue
@@ -10,16 +10,19 @@
 </template>
 
 <script>
+import Vue from "vue";
+import Component from "vue-class-component";
 {{#unless router}}
-import HelloWorld from './components/HelloWorld'
-
+import HelloWorld from './components/HelloWorld.vue'
 {{/unless}}
-export default {
+
+@Component({
   name: 'app'{{#router}}{{else}},
   components: {
     HelloWorld
   }{{/router}}
-}
+})
+export default class App extends Vue {}
 </script>
 
 <style>
