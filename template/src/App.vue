d3c604becb91163bd49179a219bda10ba9ea9bbb
Lucas van Dongen <lucas@slik.eu>
2018-06-20 13:56:42 +0200

    Template: Moved less to seperate files, opted to use vue-property-decorator for its watch functionality

--- template/src/App.vue
+++ template/src/App.vue
@@ -10,8 +10,8 @@
 </template>
 
 <script lang="ts">
-	import Vue from "vue";
-	import Component from "vue-class-component";
+	import { Component, Emit, Inject, Model, Prop, Provide, Vue, Watch } from 'vue-property-decorator'
+
 	{{#unless router}}
 	import HelloWorld from './components/HelloWorld.vue'
 	{{/unless}}
@@ -22,18 +22,38 @@
 		HelloWorld
 	}{{/router}}
 	})
-	export default class App extends Vue {}
+	export default class App extends Vue {
+		// Data()
+		msg: string = 'App was created!'
+		
+		// Computed
+		get mtdMessage(): string {
+			return this.greet()
+		}
+
+		// Methods
+		greet(): string {
+			return `Mounted: ${this.msg}`
+		}
+
+		// Watch
+		@Watch('msg')
+		OnMessageChange(val: string, oldVal: string): void {
+			console.log(`Message was changed from ${oldVal} to ${val}`)
+		}
+
+		// Created, Mounted etc
+		mounted() {
+			console.log(this.mtdMessage)
+			this.msg = 'Nice'
+		}
+	}
 </script>
 
 <style lang="less">
+	// Main for general styling that apply to everything
 	@import 'less/main';
 
-	#app {
-		font-family: 'Avenir', Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
-	}
+	// Files in /components/ folder for component styling
+	@import 'less/components/App';
 </style>
--- template/src/components/HelloWorld.vue
+++ template/src/components/HelloWorld.vue
@@ -21,33 +21,15 @@
 </template>
 
 <script lang="ts">
-	import Vue from "vue";
-	import Component from "vue-class-component";
+	import { Component, Emit, Inject, Model, Prop, Provide, Vue, Watch } from 'vue-property-decorator'
 
 	@Component
 	export default class HelloWorld extends Vue {
-		msg: string = 'Welcome to Your Vue.js App'
+		msg: string = 'Let\'s get started with this Vue app!'
 	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
-	h1,
-	h2 {
-		font-weight: normal;
-	}
-
-	ul {
-		list-style-type: none;
-		padding: 0;
-		
-		li {
-			display: inline-block;
-			margin: 0 10px;
-		}
-	}
-
-	a {
-		color: #42b983;
-	}
+	@import '../less/components/HelloWorld';
 </style>
